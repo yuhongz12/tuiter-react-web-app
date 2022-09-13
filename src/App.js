c86be527aa0d6864378d98249715c54d5eeb3864
yuhongz12 <yuhong.zhao12@gmail.com>
2022-09-13 14:15:55 -0400

    starting a2 branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>THIS IS ASSIGNMENT 2 BRANCH</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
