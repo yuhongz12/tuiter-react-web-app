f848b1901d5696780c822d29f61fb822e6a5833e
yuhongz12 <yuhong.zhao12@gmail.com>
2022-10-20 16:24:39 -0400

    complete a6 lab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Labs from "./labs";
+import HelloWorld from "./labs/a6/hello-world";
+import Tuiter from "./tuiter";
+import {BrowserRouter} from "react-router-dom";
+import {Route, Routes} from "react-router";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>THIS IS ASSIGNMENT 2 BRANCH</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <h2>THIS IS ASSIGNMENT 2 BRANCH</h2>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+      <BrowserRouter>
+          <div className={'container'}>
+              <Routes>
+                  <Route index
+                         element={<Labs/>}/>
+                  <Route path="/hello"
+                         element={<HelloWorld/>}/>
+                  <Route path="/tuiter"
+                         element={<Tuiter/>}/>
+
+              </Routes>
+          </div>
+      </BrowserRouter>
+
   );
 }
 
---
+++ src/labs/a6/index.js
@@ -0,0 +1,26 @@
+import Nav from "../../nav";
+import Classes from "./classes";
+import Styles from "./styles";
+import ConditionalOutput
+    from "./conditional-output";
+import TodoList from "./todo/todo-list";
+import TodoItem from "./todo/todo-item";
+
+
+function Assignment6() {
+    return (
+        <div>
+            <div>
+                <Nav/>
+                <h1>Assignment 6</h1>
+                <TodoList/>
+                <TodoItem/>
+                <ConditionalOutput/>
+                <Styles/>
+                <Classes/>
+            </div>
+        </div>
+    )
+}
+
+export default Assignment6;
