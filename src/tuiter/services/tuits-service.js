57d4bbe17e868cd100e6fd9ff79f2e712652c519
yuhongz12 <yuhong.zhao12@gmail.com>
2022-11-20 18:32:23 -0500

    update api link

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-const API_BASE = 'http://localhost:4000/api'
-// const API_BASE = 'https://yuhong-tuiter-node-server-app.herokuapp.com/api'
+const API_BASE = process.env.REACT_APP_API_BASE;
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
